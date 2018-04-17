2ce8687a1923f3e002a9cadb908708c0060889df
Jordan Toro <jordant0724@gmail.com>
2018-04-17 06:52:50 -0400

    add logout and admin authentication middleware

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,13 @@
-var express = require('express');
-var	router  = express.Router({mergeParams: true});
-var Event   = require('../models/event');
-var User    = require('../models/user');
-var multer  = require('multer');
-var ejs     = require('ejs');
+var express  = require('express');
+var	router   = express.Router({mergeParams: true});
+var Event    = require('../models/event');
+var User     = require('../models/user');
+var multer   = require('multer');
+var ejs      = require('ejs');
+var passport = require('passport');
 
 
-router.get('/admin/dashboard', function(req, res){
+router.get('/admin/dashboard', isLoggedIn, function(req, res){
 	res.render("./admin/dashboard")
 });
 
@@ -15,8 +16,16 @@ router.get('/admin', function(req, res){
 	//res.redirect('./admin/dashboard');
 });
 
-router.post('/admin', function(req, res){
+router.post('/admin', passport.authenticate("local", {
+	successRedirect: "/admin/dashboard",
+	failureRedirect: "/admin"
+}), function(req, res){
 	
+});
+
+router.get('/admin/logout', function(req, res){
+	req.logout();
+	res.redirect('/admin');
 });
 
 //Register Routes for future updates
@@ -37,5 +46,11 @@ router.post('/admin/register', function(req, res){
 		}
 	});
 });*/
+
+function isLoggedIn(req, res, next){
+	if(req.isAuthenticated()){
+		return next ();
+	} res.redirect('/admin');
+}
 
 module.exports = router;
